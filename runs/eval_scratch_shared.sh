dfc8b88eced7834f9934fe7aaef6bb895030078c
Stan Fris <scur1710@int6.local.snellius.surf.nl>
2025-11-23 21:58:20 +0100

    add files

--- runs/eval_scratch_shared.sh
+++ runs/eval_scratch_shared.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-seeds=(456)
+seeds=(123)
 
 dataset='topiocqa'
-model='mistral_infonce_005_1763376578' # options are 'mistral', 'mistral_improved', 'mistral_rewrites', 'splade_vanilla'
+model='mistral_infonce_000_1763636213' # options are 'mistral', 'mistral_improved', 'mistral_rewrites', 'splade_vanilla'
 # checkpoint_ids=(4544 9088 13632 18176 22720) # specify checkpoint ids or leave empty for final model
 checkpoint_ids=(2272 4544 6816 9088 11360 13632 15904) # specify checkpoint ids or leave empty for final model
 
