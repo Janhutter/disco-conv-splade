2aee64e8b959321a012b890a7eaba3101b4c7cde
Janhutter <89267625+Janhutter@users.noreply.github.com>
2025-12-18 12:08:01 +0100

    changed readme

--- runs/eval.sh
+++ runs/eval.sh
@@ -4,7 +4,7 @@
 seeds=(123)
 
 dataset='ikat23'
-model='mistral_indomain' # options are 'mistral', 'mistral_improved', 'mistral_rewrites', 'splade_vanilla', 't5_rewrites', 'human_rewrites'
+model='mistral' # options are 'mistral', 'mistral_rewrites', 'splade_vanilla', 't5_rewrites', 'human_rewrites'
 
 for seed in ${seeds[@]}; do
     sbatch runs/eval.job $seed $dataset $model
